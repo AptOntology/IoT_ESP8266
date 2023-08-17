d82164de82d079db5537c2cce7a7898210b68190
AptOntology <31258046+AptOntology@users.noreply.github.com>
2023-08-17 00:28:17 -0500

    Update IoT_ESP8266.ino

--- IoT_ESP8266.ino
+++ IoT_ESP8266.ino
@@ -1,7 +1,10 @@
 /**
 ESP8266 Client
 
-connects to wifi, sets initial settings, then get unique settings from serial input, internal web site input, or internet HTTP server, lastly run the settings on the given inverval
+1. Connects to wifi
+2. Gets settings from either initial hardcoded, serial input, internal web site input, or internet HTTP server
+3. Run the settings on the given inverval
+4. If no wifi then become access point for local control
 
 Initial Settings:
 @update,enable=1,time=300000,lastRun=0;
@@ -10,12 +13,21 @@ Initial Settings:
 
 Supported Settings
 @analogRead,enable=1,pin=A0,time=30000,lastRun=0;
-@digitalRead
+@digitalRead,enable=1,pin=12,time=30000,lastRun=0;
+@digitalWrite,enable=1,pin=12,type=HIGH,time=1000,lastrun=0;
+@AccessPoint,enable=1,be=true; 
+@update,enable=1,time=300000,lastRun=0; //get update from http server
+@loop,enable=1,time=1000; // this controls interval between RunSettings in loop()
+@serialRead,enable=1; 
+@debug,enable=1,be=true;
 
-Todo support for multiple @analogRead, @digitalRead per pin, currently allows 1
+Todo 
+support for multiple @analogRead, @digitalRead per pin, currently allows 1
+settings to control internal web services separate from access point
+output setting results to internal html page for realtime display 
 
 Native libraries 
-V 0.2
+V 0.3
 
 */
 #include <Arduino.h>
@@ -45,8 +57,7 @@ bool isDebugOut = true; //Enable verbose debug logging
 bool runOnce = false;
 bool enableAP = false; 
 bool autonomous; 
-String theStatus; // short status update for instant dashboard reporting? TODO currently last function to write Out
-// 60000 millis = 1 min
+String theStatus; // short status update for instant dashboard reporting? appends status?
 
 void setup() {
   Serial.begin(115200);
@@ -62,13 +73,12 @@ void loop() {
   if ((WiFiMulti.run() == WL_CONNECTED)) {
     if(runOnce != true) { diag(); runOnce = true; }
     RunCommands();
-    
   }
   else
   {
     // No WIfi? be autonomous AP for a while 
     isDebugOut = autonomous = enableAP = true; 
-    theStatus = "No Wifi!"; // ??TODO??
+    theStatus = "No Wifi!";
   }
   delay(loopInterval);
   LoopAP(); // Be an AP if enableAP else handle web server?
@@ -92,8 +102,9 @@ void UpdateSettings(String line)
         // if incoming lastRun = 0 then set to 0, update lastRun cycle with millis if between 1-10, and set explicit if greater than 10
         if(line.indexOf("lastRun") > 0)
         {
-          String incomingLastRun1 = line.substring(line.indexOf("lastRun="),line.indexOf(";")); // @cmd,pin=1,time=1,lastRun=123;
-          String incomingLastRun = incomingLastRun1.substring(incomingLastRun1.indexOf("=")+1,incomingLastRun1.indexOf(";")); //123
+          //String incomingLastRun1 = line.substring(line.indexOf("lastRun="),line.indexOf(";")); // @cmd,pin=1,time=1,lastRun=123;
+          //String incomingLastRun = incomingLastRun1.substring(incomingLastRun1.indexOf("=")+1,incomingLastRun1.indexOf(";")); //123
+          String incomingLastRun = GetSettingValue(line,"lastRun");
           if(incomingLastRun == "0") // update as-is, always runs on next interval
           {
             Out("debug UpdateSettings incomingLastRun ", (String)incomingLastRun);
@@ -162,44 +173,44 @@ String GetSettingValue(String toRun, String name) //incoming toRun and name of c
   }
   return thingStr; 
 }
-
+bool isSettingsInit; 
 void initSettings()
 {
   espSettings[0] = "@update,enable=1,time=300000,lastRun=0;"; 
   espSettings[1] = "@serialRead,enable=1;"; 
-  espSettings[2] = "@debug,enable=1,false;";
-  theStatus = "Settings initialized!"; // ??TODO??
+  espSettings[2] = "@debug,enable=1,be=false;";
+  theStatus = "Initialized!";
+  isSettingsInit = true;
 }
 
 void RunCommands() {
-    if(espSettings[0].indexOf(",") < 1) { initSettings(); }  // if settings empty then initialize
+    if(isSettingsInit == false) { initSettings(); }  // if settings empty then initialize
     for (int i = 0; i < (sizeof(espSettings) / sizeof(espSettings[0])); i++)
     {
         if (espSettings[i] != "")
         {
           String toRun = espSettings[i];
-          String enableStr1 = (toRun.substring(toRun.indexOf("enable"), toRun.indexOf(";"))); //enable=1,time=30000,lastRun=0;
-          String enableStr = (enableStr1.substring(enableStr1.indexOf("=")+1,enableStr1.indexOf("=")+2)); // 1
+          String enableStr = GetSettingValue(toRun,"enable");
           if(enableStr == "1" || enableStr != "0")
           {
             Out("debug RunCommands enabled ",toRun);
-            if(toRun.startsWith("@analogRead")) 
+            if(toRun.startsWith("@analogRead")) //@analogRead,enable=1,pin=A0,returnMaxMin=1,time=10000,lastRun=0;
             {
               String pinStr = GetSettingValue(toRun,"pin");
               String intervalStr = GetSettingValue(toRun,"time"); 
               String lastRunStr = GetSettingValue(toRun,"lastRun");
-              Out("debug RunCommands analogRead pin ",pinStr);
-              Out("debug RunCommands analogRead time ",intervalStr);
-              Out("debug RunCommands analogRead lastRun ",lastRunStr);// work!
-          
-              if(pinStr.indexOf("A") != -1) // this must start with A
+              String returnMaxMinStr = GetSettingValue(toRun,"returnMaxMin");
+              if(pinStr.indexOf("A") != -1 && pinStr.length() <= 3) // this must start with A and must be less than 3
               {
-                // Get time interval to run and last run time. If time to run then run
-                if(millis() > atoi(lastRunStr.c_str()) + atoi(intervalStr.c_str()))
+                if(millis() > atoi(lastRunStr.c_str()) + atoi(intervalStr.c_str()))  // Get time interval to run and last run time. If time to run then run
                 {
                   String analogValue = (String)analogRead(atoi(pinStr.c_str()));
                   Out("RunCommands",((String)"analogRead " + (String)pinStr + " value : " + analogValue));
                   if(analogValue != "1024" && analogValue != "0")
+                  {
+                    SendData("analogRead",analogValue);
+                  } 
+                  else if(returnMaxMinStr == "1")
                   {
                     SendData("analogRead",analogValue);
                   }
@@ -208,104 +219,120 @@ void RunCommands() {
               }
               else
               {
-               Out("debug RunCommands analogRead pinStr no A : ", pinStr);
+               Out("debug RunCommands analogRead pinStr invalid : ", pinStr);
               }
             }
-            if(toRun.startsWith("@digitalRead"))
+            if(toRun.startsWith("@digitalRead")) // toRun : @digitalRead,pin=0,time=1000,lastrun=12345;
             {
-              // toRun : @digitalRead,pin=0,time=1000,lastrun=12345;
-              String pinStr1 = toRun.substring(toRun.indexOf("pin"),toRun.lastIndexOf(";")); // pin=0,time=1000/t,lastrun=12345;
-              String pinStr = pinStr1.substring(pinStr1.indexOf("=")+1,pinStr1.indexOf(",")); // 0
-              pinMode(atoi(pinStr.c_str()), INPUT);
-              String analogValue = (String)digitalRead(atoi(pinStr.c_str()));
-              Out("RunCommands",((String)"digitalRead " + (String)pinStr + " value : " + analogValue));
-              SendData("digitalRead",analogValue);
-            }
-            if(toRun.startsWith("@digitalWrite"))               // toRun : @digitalWrite,pin=0,type=HIGH,time=1000,lastrun=12345;
-            {
-              String pinStr1 = toRun.substring(toRun.indexOf("pin"),toRun.lastIndexOf(";")); // pin=0,time=1000/t,lastrun=12345;
-              String pinStr = pinStr1.substring(pinStr1.indexOf("=")+1,pinStr1.indexOf(",")); // 0//
-              String typeStr1 = toRun.substring(toRun.indexOf("type"),toRun.lastIndexOf(";")); // type=HIGH,time=1000,lastrun=12345;
-              String typeStr = typeStr1.substring(typeStr1.indexOf("=")+1,typeStr1.indexOf(",")); // HIGH
-            
-              pinMode(atoi(pinStr.c_str()), OUTPUT);
-              if(typeStr == "HIGH")
+              String pinStr = GetSettingValue(toRun,"pin");
+              String intervalStr = GetSettingValue(toRun,"time"); 
+              String lastRunStr = GetSettingValue(toRun,"lastRun");
+
+              if(pinStr.length() >= 1 && pinStr.length() <= 1) // this must be =>1 and <=2
               {
-                digitalWrite(atoi(pinStr.c_str()),HIGH);
+                if(millis() > atoi(lastRunStr.c_str()) + atoi(intervalStr.c_str()))  // Get time interval to run and last run time. If time to run then run
+                {
+                  pinMode(atoi(pinStr.c_str()), INPUT);
+                  String digitalValue = (String)digitalRead(atoi(pinStr.c_str()));
+                  Out("RunCommands",((String)"digitalRead " + (String)pinStr + " value : " + digitalValue));
+                  SendData("digitalRead",digitalValue);
+                  UpdateSettings("@digitalRead,pin="+pinStr+",time="+intervalStr+",lastRun="+millis()+";");
+                }
               }
-              else if(typeStr == "LOW")
+              else
               {
-                digitalWrite(atoi(pinStr.c_str()),LOW);
+                Out("debug RunCommands digitalRead pinStr invalid : ", pinStr);
               }
-              Out("RunCommands",((String)"digitalWrite " + (String)pinStr + " " + (String)typeStr));
             }
-            if(toRun.startsWith("@AccessPoint"))
+            if(toRun.startsWith("@digitalWrite"))   // toRun : @digitalWrite,enable=1,pin=0,type=HIGH,time=1000,lastrun=12345;
             {
-              if(toRun.indexOf("true") >= 0)
+              String pinStr = GetSettingValue(toRun,"pin");
+              String intervalStr = GetSettingValue(toRun,"time"); 
+              String lastRunStr = GetSettingValue(toRun,"lastRun");
+              String typeStr = GetSettingValue(toRun,"type");
+
+              if(pinStr.length() >= 1 && pinStr.length() <= 1) // pin must be =>1 and <=2
               {
-                enableAP = true;
+                if(millis() > atoi(lastRunStr.c_str()) + atoi(intervalStr.c_str()))  // Get time interval to run and last run time. If time to run then run
+                {
+                  pinMode(atoi(pinStr.c_str()), OUTPUT);
+                  if(typeStr == "HIGH")
+                  {
+                    digitalWrite(atoi(pinStr.c_str()),HIGH);
+                  }
+                  else if(typeStr == "LOW")
+                  {
+                    digitalWrite(atoi(pinStr.c_str()),LOW);
+                  }
+                  Out("RunCommands",((String)"digitalWrite " + (String)pinStr + " " + (String)typeStr));
+                }
+              }
+              else
+              {
+                Out("debug RunCommands digitalWrite pinStr invalid : ", pinStr);
+              }
+            }
+            if(toRun.startsWith("@AccessPoint")) //@AccessPoint,enable=1,be=true; // does this need an interval or can we flipflop the boolean?
+            {
+              String beStr = GetSettingValue(toRun,"be");
+              if(beStr == "true")
+              {
+                if(!enableAP)
+                {
+                  enableAP = true;
+                  SetupAP();
+                  Out("debug RunSettings AccessPoint", "true");
+                }
               }
               else
               {
                 enableAP = false;
+                WiFi.mode(WIFI_STA);
+                Out("debug RunSettings AccessPoint", "false");
               }
             }
-            if(toRun.startsWith("@loop"))
+            if(toRun.startsWith("@loop")) //@loop,enable=1,time=1000;
             {
-              // toRun : @loop,time=10000,enable=1;
-              // todo: add enable, refactor to include time string rather than = in the next line
-              long newLoopInterval = atoi((toRun.substring(toRun.lastIndexOf("=")+1,toRun.length())).c_str());
-              //Out("debug RunCommand Loop interval", ("Loop interval set to "+ (String)newLoopInterval));
+              String intervalStr = GetSettingValue(toRun,"time");
+              long newLoopInterval = atoi(intervalStr.c_str());
               if(loopInterval != newLoopInterval)
               {
                 loopInterval = newLoopInterval;
+                Out("debug RunSettings loop set ", (String)newLoopInterval);
               }
             }
             if(toRun.startsWith("@update")) //@update,enable=1,time=10000,lastRun=0;
             {            
-              //interval time
-              String intervalStr1 = toRun.substring(toRun.indexOf("time"),toRun.indexOf(";")); // time=10,lastrun=0;
-              String intervalStr = intervalStr1.substring(intervalStr1.indexOf("=")+1,intervalStr1.indexOf(",")); // 10
-
-              //Out("debug RunCommandsupdate intervalStr1", intervalStr1);
-              //Out("debug RunCommandsupdate intervalStr", intervalStr);
-
-              //last run time
-              String lastRunStr1 = toRun.substring(toRun.indexOf("lastRun"),toRun.indexOf(";")); // lastRun=0;
-              String lastRunStr = lastRunStr1.substring(lastRunStr1.indexOf("=")+1,lastRunStr1.indexOf(";")); // 0
-
-              // Get time interval to run and last run time. If time to run then run
+              String intervalStr = GetSettingValue(toRun,"time");
+              String lastRunStr = GetSettingValue(toRun,"lastRun");
               if(millis() > atoi(lastRunStr.c_str()) + atoi(intervalStr.c_str()))
               {
                 Out("debug RunCommandsupdate ", "Running GetSetupConfig()");
                 if(GetSetupConfig())
                 {
-                  // received update
-                  //Out("debug RunCommands GetSetupConfig returned true", " ");
+                  Out("debug RunCommands GetSetupConfig returned", "true");
                 }
                 else
                 {
                   // no @update from server, update the LastRun to now and keep the current intervalStr
-                  //Out("debug RunCommands GetSetupConfig returned false", " ");
                   if(intervalStr == "") { intervalStr = "45000";} // if no interval then set to 45 seconds
-                  UpdateSettings("@update,enable=1,time="+intervalStr+",lastRun="+millis()+";");
-                  //UpdateSettings("@analogRead,pin="+pinStr+",time="+intervalStr+",lastRun="+millis()+";");
-              
+                  UpdateSettings("@update,enable=1,time="+intervalStr+",lastRun="+millis()+";");              
                 }
               }
             }
-            if(toRun.startsWith("@debug"))               //@debug,enable=1,mode=false;
+            if(toRun.startsWith("@debug"))  //@debug,enable=1,be=true;
             {
-              if(toRun.indexOf("false") > 0)
+              String beStr = GetSettingValue(toRun,"be");
+              if(beStr == "false")
               {
                 isDebugOut = false;
               }
-              else 
+              else if(beStr == "true")
               {
                 isDebugOut = true;
               }
             }
-            if(toRun.startsWith("@serialRead"))
+            if(toRun.startsWith("@serialRead")) //@serialRead,enable=1;
             {
               if(Serial.available())
               {
@@ -313,7 +340,6 @@ void RunCommands() {
                 String readStr = "";
                 readStr = Serial.readString();
                 delay(2);
-              
                 if(readStr != "")
                 {
                   readStr = readStr.substring(0,readStr.length()-1);
@@ -322,7 +348,7 @@ void RunCommands() {
                   if(readStr.startsWith("@"))
                   {
                     UpdateSettings(readStr);
-                    theStatus = "Settings SERIAL configured!"; // ??TODO??
+                    theStatus = "Configured by serial!"; // ??TODO??
                   }
                   else
                   {
@@ -411,14 +437,19 @@ bool GetSetupConfig() {
     // at index should always be 1, semiIndex should always be the length of the string, the last entry may or may not have a ; (lol)
     String theCmd = subPayload.substring(atIndex,semiIndex+1);
 
-    if(theCmd.indexOf("@update") != -1) 
+    if(theCmd.indexOf("@update") != -1)  // if the command is not update then toReturn is true? I know I put this here for some reason though... todo
     {
-      toReturn = true;
+      toReturn = true; 
     }
-    if(theCmd.indexOf("0") != 0) // filter 0 results from commands.php 
+    if(theCmd.indexOf("@") == 0) // every setting must start with @, if >= or otherwise then its broken setting, could also check for trailing ; while we're here
     {
       UpdateSettings(theCmd);
-      theStatus = "Settings SERVER configured!"; // ??TODO??
+      theStatus = "Configured by server!";
+      toReturn = true;
+    }
+    else
+    {
+      Out("debug GetSetupConfig invalid setting format", theCmd);
     }
 
     if(semiIndex == 0 || semiIndex == -1)
@@ -432,7 +463,6 @@ bool GetSetupConfig() {
 
 void Out(String function, String message) {
   bool isOut;
-  //theStatus = function; // ??TODO??
   if(function.startsWith("debug") && isDebugOut) {
     isOut = true;
   }
